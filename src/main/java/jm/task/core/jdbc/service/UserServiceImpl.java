6441ab62950a4316f5f74f4a46533b1fc2bc4559
Admin <blackwolf.2000@mail.ru>
2024-08-22 18:44:20 +0300

    JDBC

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -11,7 +11,7 @@ public class UserServiceImpl implements UserService {
    private final UserDao userDao;
 
     public UserServiceImpl() {
-        userDao = new UserDaoHibernateImpl();
+        userDao = new UserDaoJDBCImpl();
     }
 
     public void createUsersTable() {
